5bf3e77b7d47c8dc17084006989f291b8c944800
joel <110068422+joelombacode@users.noreply.github.com>
2026-09-20 22:59:36 +0000

    second commit setting projet

---
+++ student_manager/build/CMakeFiles/StudentManeger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentManeger.dir/src/main.cpp.o"
+  "CMakeFiles/StudentManeger.dir/src/main.cpp.o.d"
+  "StudentManeger"
+  "StudentManeger.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudentManeger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ student_manager/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout <<"Student Manager Hello World!"<<std::endl;
+    return 0;
+}
